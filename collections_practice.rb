bcf8b83fcd127f28381829696d0765dc7d95d574
motti770 <mottigottlieb@gmail.com>
2016-02-22 23:02:32 +0000

    nitrous-backup at Mon Feb 22 23:02:32 UTC 2016

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
 # your code goes here
+def begins_with_r(tools)
+
+  tools.all? do |a|
+    a[0] == "r"
+  end
+  end
+
+def contain_a(tool)
+  tool.select do |ltr|
+    ltr.include?("a")
+  end
+end
+
+def first_wa(tool)
+  tool.find { |i| if i.is_a? String then i.start_with?("wa") end }
+
+end
+
+def remove_non_strings(arr)
+  arr.select { |i| i.is_a? String }
+end
+
+def count_elements(words)
+  counts = Hash.new 
+
+words.each do |word|
+  counts[word] += 1
+end
+
+end
